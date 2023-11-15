bad04fb5f602ec63a17767db197ff585dff80445
JaoodxD <jaood96@gmail.com>
2023-11-15 16:25:51 +0200

    Add 200ms server response throttling

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import serve from './server/server.js'
 
 const path = join(import.meta.url, './datasets/AnimeQuotes.csv')
 
-const { stop } = serve({ port: 3000, path })
+const { stop } = serve({ port: 3000, path, throttle: 200 })
 
 process.on('SIGINT', stop)
 process.on('SIGTERM', stop)
