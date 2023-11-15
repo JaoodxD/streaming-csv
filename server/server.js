4cdbb568d7c5a3368aacd34ea04cf6ac3425ce60
JaoodxD <jaood96@gmail.com>
2023-11-15 14:40:33 +0200

    Add throttling

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 import { createServer } from 'node:http'
 import { createReadStream } from 'node:fs'
-import { pipeline } from 'node:stream/promises'
+import { TransformStream } from 'node:stream/web'
+import { Readable, Transform, Writable } from 'node:stream'
+import { setTimeout as wait } from 'node:timers/promises'
 import csvtojson from 'csvtojson'
-export default function serve ({ port, path }) {
+export default function serve ({ port, path, throttle }) {
   const server = createServer(async function (request, response) {
     const csv = createReadStream(path)
-    await pipeline(csv, csvtojson(), response)
+    await Readable.toWeb(csv)
+      .pipeThrough(Transform.toWeb(csvtojson()))
+      .pipeThrough(
+        new TransformStream({
+          async transform (chunk, controller) {
+            const data = JSON.parse(Buffer.from(chunk))
+            const mappedData = JSON.stringify({ line: data.Quote }) + '\n'
+            if (throttle) await wait(throttle)
+            controller.enqueue(mappedData)
+          }
+        })
+      )
+      .pipeTo(Writable.toWeb(response))
   })
 
   server.listen(port).on('listening', () => {
