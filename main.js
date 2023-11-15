7d1fba6d8d47bb3db4a68d60d18598a3cdb66788
JaoodxD <jaood96@gmail.com>
2023-11-15 17:41:57 +0200

    Add collection of presets for datasets

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 import { join } from 'desm'
 import serve from './server/server.js'
 
-const path = join(import.meta.url, './datasets/FACEBOOK_REVIEWS.csv')
-const fields = {
-  title: 'author_name',
-  description: 'review_text'
+const datasets = {
+  facebook: {
+    path: join(import.meta.url, './datasets/FACEBOOK_REVIEWS.csv'),
+    fields: {
+      title: 'author_name',
+      description: 'review_text'
+    }
+  },
+  anime: {
+    path: join(import.meta.url, './datasets/AnimeQuotes.csv'),
+    fields: {
+      title: 'Character',
+      description: 'Quote'
+    }
+  }
 }
 
-const { stop } = serve({ port: 3000, throttle: 100, path, fields })
+const { stop } = serve({ port: 3000, throttle: 100, ...datasets.facebook })
 
 process.on('SIGINT', stop)
 process.on('SIGTERM', stop)
