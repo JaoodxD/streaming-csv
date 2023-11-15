5649a240c7e22a9f8a83664dfb16e7ef8aaf952b
JaoodxD <jaood96@gmail.com>
2023-11-15 15:48:39 +0200

    Add cors policy

--- server/server.js
+++ server/server.js
@@ -4,26 +4,42 @@ import { TransformStream } from 'node:stream/web'
 import { Readable, Transform, Writable } from 'node:stream'
 import { setTimeout as wait } from 'node:timers/promises'
 import csvtojson from 'csvtojson'
+
 export default function serve ({ port, path, throttle }) {
   const server = createServer(async function (request, response) {
-    const csv = createReadStream(path)
-    await Readable.toWeb(csv)
-      .pipeThrough(Transform.toWeb(csvtojson()))
-      .pipeThrough(
-        new TransformStream({
-          async transform (chunk, controller) {
-            const data = JSON.parse(Buffer.from(chunk))
-            const mappedData = JSON.stringify({ line: data.Quote }) + '\n'
-            if (throttle) await wait(throttle)
-            controller.enqueue(mappedData)
-          }
-        })
-      )
-      .pipeTo(Writable.toWeb(response))
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': '*'
+    }
+    if (request.method === 'OPTIONS') {
+      response.writeHead(204, headers)
+      response.end()
+      return
+    }
+
+    try {
+      response.writeHead(headers)
+      const csv = createReadStream(path)
+      await Readable.toWeb(csv)
+        .pipeThrough(Transform.toWeb(csvtojson()))
+        .pipeThrough(
+          new TransformStream({
+            async transform (chunk, controller) {
+              const data = JSON.parse(Buffer.from(chunk))
+              const mappedData = JSON.stringify({ line: data.Quote }) + '\n'
+              if (throttle) await wait(throttle)
+              controller.enqueue(mappedData)
+            }
+          })
+        )
+        .pipeTo(Writable.toWeb(response))
+    } catch (error) {
+      console.log('something happen', error)
+    }
   })
 
   server.listen(port).on('listening', () => {
-    console.log('listening on port ', port)
+    console.log('listening on port', port)
   })
 
   return { stop }
